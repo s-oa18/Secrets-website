b4a63fa82c2d66c48f16cbc5c458454334ab265d
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-11-17 03:48:52 +0000

    Level 1 - Only Username and Password

--- app.js
+++ app.js
@@ -33,6 +33,38 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
+app.post("/register", function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save(function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
 app.listen("3000", function (req, res) {
   console.log("Server has started on port 3000.");
 });
