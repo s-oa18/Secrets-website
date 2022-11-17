bea3733fee6f671661f7863809a3f108730d27ba
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-11-17 03:37:21 +0000

    mongoose database integrattion

--- app.js
+++ app.js
@@ -3,12 +3,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const { setTheUsername } = require("whatwg-url");
 
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
