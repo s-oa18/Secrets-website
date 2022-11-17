0b5b4cc0edfd9890daba2c2d9f946397447ef197
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2022-11-17 17:13:08 +0000

    level 2 - Encryption. Added environment vars

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 //jshint esversion:6
 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const { setTheUsername } = require("whatwg-url");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -17,6 +18,11 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+});
+
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ["password"],
 });
 
 const User = new mongoose.model("User", userSchema);
